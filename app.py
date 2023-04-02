72373f206a335427e429773a25d4eb754641a1f6
Abner <59564617+ZhangYichang235@users.noreply.github.com>
2023-04-02 02:06:01 -0230

    new codes!!!!

---
+++ app.py
@@ -0,0 +1,173 @@
+import customtkinter as ctk
+import bug_fix
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Box Bot")
+        self.minsize(400, 600)
+        self.maxsize(400, 600)
+
+        self.dimensions = []
+        self.dimensions_fragile = []
+        self.tf = False
+        
+## Text ##:
+        self.title = ctk.CTkLabel(
+            master=self,
+            text= "Box Bot",
+            width=120,
+            height=25,
+            font=("Arial Rounded MT Bold", 50,"bold")
+            )
+        
+        self.lengthtext = ctk.CTkLabel(
+            master=self,
+            text= "Insert Length:",
+            font=("", 25,"bold")
+            )
+        
+        self.widthtext = ctk.CTkLabel(
+            master=self,
+            text= "Insert Width:",
+            font=("", 25,"bold")
+            )
+        
+        self.heighttext = ctk.CTkLabel(
+            master=self,
+            text= "Insert Height:",
+            font=("", 25,"bold")
+            )
+# Entries #
+        self.lengthinput = ctk.CTkEntry(
+            master=self,
+            height=25,
+            width=200,
+            border_width=2,
+            corner_radius=10
+        )
+        self.widthinput = ctk.CTkEntry(
+            master=self,
+            height=25,
+            width=200,
+            border_width=2,
+            corner_radius=10
+        )
+        self.heightinput = ctk.CTkEntry(
+            master=self,
+            height=25,
+            width=200,
+            border_width=2,
+            corner_radius=10
+        )
+
+## Option Menu ##       
+        self.fragile = ctk.CTkOptionMenu(
+            master=self,
+            values=["Non-Fragile", "Fragile"]
+        )
+
+
+## Buttons ##
+        self.addtolist= ctk.CTkButton(
+            master=self,
+            width=200,
+            height=32,
+            corner_radius=10,
+            text="Add to List",
+            command=self.addtolist
+        )
+        self.result= ctk.CTkButton(
+            master=self,
+            width=300,
+            height=70,
+            corner_radius=10,
+            text="Calculate Map",
+            command=self.open_map
+        )
+        self.cancel= ctk.CTkButton(
+            master=self,
+            width=300,
+            height=25,
+            corner_radius=10,
+            text="Delete last item",
+            command=self.delete
+        )
+
+## Textbox ##
+        self.textbox = ctk.CTkTextbox(
+            master=self, 
+            width=400,
+            height=275,
+            corner_radius=0
+        )
+        self.textbox.configure(state="disabled")
+
+        self.title.grid(row=0, column=0, columnspan=2)
+
+        self.lengthtext.grid(row=1, column=0, pady=5)
+        self.widthtext.grid(row=2, column=0, pady=5)
+        self.heighttext.grid(row=3, column=0, pady=5)
+
+        self.lengthinput.grid(row=1, column=1, pady=5, sticky="w")
+        self.widthinput.grid(row=2, column=1, pady=5, sticky="w")
+        self.heightinput.grid(row=3, column=1, pady=5)
+        
+        self.fragile.grid(row=4, column=0,pady=5)
+        self.addtolist.grid(row=4, column=1, pady=5)
+
+        self.textbox.grid(row=5, column=0, columnspan=2)
+
+        self.result.grid(row=6, column=0, columnspan=2, pady=5)
+        self.cancel.grid(row=7, column=0, columnspan=2, pady=5)
+
+    def addtolist(self):
+        #adds input text into textbox
+        self.textbox.configure(state="normal")
+        global tf
+
+        l = float(self.lengthinput.get())
+        w = float(self.widthinput.get())
+        h = float(self.heightinput.get())
+
+        dim = [l, w, h]
+
+        #appends dimensions into global list
+
+        if self.fragile.get() == "Fragile":
+            tf = True
+            self.textbox.insert("0.0" ,f"FRAGILE:{l} x {w} x {h}\n")
+            self.dimensions_fragile.append(dim)     
+        else:
+            tf = False
+            self.textbox.insert("0.0" ,f":{l} x {w} x {h}\n") 
+            self.dimensions.append(dim)
+
+                
+        self.textbox.configure(state="disabled")
+
+
+    def delete(self):       
+        global dimensions
+        #delete added line
+        self.textbox.configure(state="normal")
+        self.textbox.delete("1.0","2.0")
+        self.textbox.configure(state="disabled")
+        #delete recent dimensions from list
+        dimensions.pop(-1)
+        print(dimensions)
+        
+
+    def open_map(self):
+        if self.tf == True:
+            bug_fix.main(self.tf, self.dimensions_fragile)
+        if self.tf == False:
+            bug_fix.main(self.tf, self.dimensions)
+
+if __name__ == "__main__":
+    app = App()
+    app.grid_rowconfigure(0)
+    app.grid_columnconfigure(0,weight=1)
+
+    app.mainloop()
---
+++ bug_fix.py
@@ -0,0 +1,147 @@
+import pygame
+import random
+
+from math import *
+
+# set up pygame window
+#                         lxwxh
+def card_board_box_net_lw(dimension, fragiletf, ear_size=2):
+    if fragiletf:
+        length = (2*dimension[0] + 2) + (2*dimension[2] + 2) + (ear_size)
+        width = (2*dimension[2] + 2) + (2*ear_size) + (dimension[1] + 2)
+
+    if not fragiletf:
+        length = (2*dimension[0]) + (2*dimension[2]) + (ear_size)
+        width = (2*dimension[2]) + (2*ear_size) + (dimension[1])
+
+    errorx = ceil(length) - length
+    errory = ceil(width) - width
+
+    return ((length, width), errorx, errory)
+
+
+# define rectangle class
+class Rectangle:
+    def __init__(self, w, h, errorx, errory, win):
+        self.width = w
+        self.height = h
+        self.x = None
+        self.y = None
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        self.win = win
+
+        self.errorx = errorx
+        self.errory = errory
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        if self.x != 0:
+            self.x = x - self.errorx
+
+        if self.y != 0:
+            self.y = y - self.errory
+    def draw(self):
+        pygame.draw.rect(self.win, self.color, (self.x, self.y, self.width-self.errorx, self.height-self.errory))
+        
+
+# list of rectangles to draw
+rectangles = []
+
+# define function to find the most valuable place on canvas
+def find_best_position(rect):
+    min_waste = float("inf")
+    best_x, best_y = None, None
+    for y in range(HEIGHT - rect.height):
+        for x in range(WIDTH - rect.width):
+            waste = 0
+            for other_rect in rectangles:
+                if x < other_rect.x + other_rect.width and x + rect.width > other_rect.x and y < other_rect.y + other_rect.height and y + rect.height > other_rect.y:
+                    waste += min(
+                        abs(x - (other_rect.x + other_rect.width)),
+                        abs(y - (other_rect.y + other_rect.height)),
+                        abs((x + rect.width) - other_rect.x),
+                        abs((y + rect.height) - other_rect.y)
+                    )
+            if waste < min_waste:
+                min_waste = waste
+                best_x, best_y = x, y
+    return best_x, best_y
+
+fragile_list = []
+non_fragile_list = []
+
+run = 1
+WIDTH = 800
+HEIGHT = 800
+
+def main(fragile, dimensions):
+    global run, WIDTH, HEIGHT
+    pygame.init()
+
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Rectangles")
+    # main game loop
+    for ind in range(0, len(dimensions)):
+        if fragile:
+            box_size_fragile = card_board_box_net_lw(dimensions[ind], fragile)
+            fragile_list.append(int(box_size_fragile[0][0]),int(box_size_fragile[0][1]))
+        
+        if not fragile:
+            box_size_non_fragile = card_board_box_net_lw(dimensions[ind], fragile)
+            non_fragile_list.append((int(box_size_non_fragile[0][0]),int(box_size_non_fragile[0][1])))
+
+
+    while True:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                # the list we input
+        if run == 1 and fragile:
+            for i in fragile_list:
+                # add a new rectangle with random dimensions
+                new_rect = Rectangle(i[0], i[1], box_size_fragile[1], box_size_fragile[2], win)
+                # find the best position for the new rectangle
+                new_x, new_y = find_best_position(new_rect)
+                new_rect.set_position(new_x, new_y)
+                # draw all rectangles
+                win.fill((255, 255, 255))
+                for rect in rectangles:
+                    rect.draw()
+                # draw new rectangle
+                new_rect.draw()
+                # update screen
+                pygame.display.update()
+                # add new rectangle to list of rectangles
+                rectangles.append(new_rect)
+                # wait for a short time to slow down the loop
+                pygame.time.wait(100)
+                run = 0
+
+        if run == 1 and not fragile:
+            for i in non_fragile_list:
+                # add a new rectangle with random dimensions
+                new_rect = Rectangle(i[0], i[1], box_size_non_fragile[1], box_size_non_fragile[2], win)
+                # find the best position for the new rectangle
+                new_x, new_y = find_best_position(new_rect)
+                new_rect.set_position(new_x, new_y)
+                # draw all rectangles
+                win.fill((255, 255, 255))
+                for rect in rectangles:
+                    rect.draw()
+                # draw new rectangle
+                new_rect.draw()
+                
+                # update screen
+                pygame.display.update()
+                
+                # add new rectangle to list of rectangles
+                rectangles.append(new_rect)
+                
+                # wait for a short time to slow down the loop
+                pygame.time.wait(100)
+
+                run = 0
